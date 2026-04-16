0b97e33e8daa8ee92cc31338f7c2c7b49364282a
PabloAznar <pablo.aznar44@gmail.com>
2026-04-16 17:24:11 +0200

    first commit

---
+++ src/test/java/com/artifact/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artifact.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
